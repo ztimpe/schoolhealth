3f203081fdc1914f41183e160f21066921d349f5
Zach Timpe <zacharytimpe@gmail.com>
2025-07-25 12:53:16 -0400

    Initial commit

---
+++ R/student_codebook.R
@@ -0,0 +1,15 @@
+#' Student Survey Codebook
+#'
+#' The codebook to the 2024-2025 Student Health Survey
+#'
+#' @format ## student_codebook
+#' A dataframe with 159 rows and 6 columns:
+#' \describe{
+#' \item{Variable}(Names of the columns in the student data)
+#' \item{Question}(The survey question)
+#' \item{response_options}(A semi-colon separated character vector of all possible
+#' response options for given Variable/Question)
+#' \item{section}(Section of the survey)
+#' \item{subset}(Whether question was for all, Elementary, or Middle/High students)
+#' \item{type}(Variable type)
+#' }
